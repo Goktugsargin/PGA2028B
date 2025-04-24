192d997069f34df6629273fa4a3bf18a745ac1d0
Goktugsargin <sargingoktug@gmail.com>
2025-04-24 15:32:56 +0300

    Update Lab04_A.py

--- Lab04_A.py
+++ Lab04_A.py
@@ -1,1 +1,13 @@
-
+hatasiz = False
+while hatasiz == False:
+    try:
+        sayi = int(input("Bir tam sayı giriniz:"))
+    except:
+        print("Hatalı Girdi")
+        hatasiz = False
+    else:
+        print("Sıkıntı YOK")
+        hatasiz = True
+    finally:
+        print("girdi hatakontrolüne bağlı tutuldu")
+    print(sayi)
